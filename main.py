5265277763c7037a97b07805cd7635195752d32e
amirjj <amir.jamshidijam@gmail.com>
2022-12-17 12:42:31 +0330

    passgen method added

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import argparse
+import string
+from random import choices
 
 
-def passgen():
-    parser = argparse.ArgumentParser(prog='Password generator',
-                                     description='A Simple program generating '
-                                                 'standard passwords',
-                                     epilog='Help text')
-    parser.add_argument('-a', '--ascii')
-    print(parser)
+def passgen(length, digit, lower, upper, pun):
+    pool = ''
+    if digit:
+        pool += string.digits
+    if pun:
+        pool += string.punctuation
+    if lower:
+        pool += string.ascii_lowercase
+    if upper:
+        pool += string.ascii_uppercase
+    if not (upper and lower and pun and digit):  # if pool == '':
+        pool += string.ascii_letters
+
+    return ''.join(choices(pool, k=length))
 
 
 if __name__ == '__main__':
@@ -16,7 +25,8 @@ if __name__ == '__main__':
                                                  'standard passwords.')
 
     parser.add_argument('length', type=int, help='Length of password')
-    parser.add_argument('--foo', action='store_false')
+    # parser.add_argument('-a', '--ascii', help='ascii', action='store_true')
+    parser.add_argument('--verbose', '-v', action='count', default=0)
     parser.add_argument('-l', '--lower', help='lowercase', action='store_true')
     parser.add_argument('-u', '--upper', help='uppercase', action='store_true')
     parser.add_argument('-p', '--pun', help='punctuation', action='store_true')
@@ -24,6 +34,7 @@ if __name__ == '__main__':
                         help='digit password', action='store_true')
 
     args = parser.parse_args()
-
     print(args)
+    pwd = passgen(args.length, args.digit, args.lower, args.upper, args.pun)
+    print(pwd)
 
